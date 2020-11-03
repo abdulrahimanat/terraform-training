b5a64144677c0934de566cba976726d5288e517a
Abdul Rahiman AT <31490398+abdulrahimanat@users.noreply.github.com>
2020-11-03 14:20:44 +0530

    Update provider.tf

--- d1/provider.tf
+++ d1/provider.tf
@@ -1,15 +1,18 @@
 terraform {
-  required_version = ">= 0.13.4"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.8.0"
+  backend "remote" {
+    organization = "abdulrahimanat"
+
+    workspaces {
+      name = "terraform-training"
     }
   }
 }
 
 provider "aws" {
-  profile = "default"
   region  = "us-west-2"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
+variable aws_access_key_id {}
+variable aws_secret_access_key {}
